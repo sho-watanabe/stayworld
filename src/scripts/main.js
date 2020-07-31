ced6f07bf67721976176bdb1148ece7905db43a0
sho-watanabe <sho-desukedo@sky.plala.or.jp>
2020-07-31 18:25:57 +0900

    webpack

---
+++ src/scripts/main.js
@@ -0,0 +1,45 @@
+import '../styles/vendors/bootstrap-reboot.css';
+import '../styles/vendors/swiper.min.css';
+import '../styles/style.scss';
+import { ScrollObserver } from './libs/scroll.js';
+import { TextAnimation,TweenTextAnimation } from './libs/text-animation.js';
+import { HeroSlider } from './libs/hero-slider.js';
+import { MobileMenu } from './libs/mobile-menu.js';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+
+    const cb = function (el, inview) {
+        if(inview) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    };
+
+    const so = new ScrollObserver('.tween-animate-title', cb);
+
+    const _inviewAnimation = function(el,inview){
+        if(inview){
+            el.classList.add('inview');
+        }else{
+            el.classList.remove('inview');
+        }
+    };
+
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+    const header = document.querySelector('.header');
+    const _navAnimation = function(el,inview){
+        if(inview){
+            header.classList.remove('triggered');
+        }else{
+            header.classList.add('triggered');
+        }
+    };
+
+    const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
+
+    new MobileMenu();
+});
---
+++ webpack.common.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { ProvidePlugin } = require("webpack");
+
+module.exports = ({ outputFile, assetFile }) => ({
+  entry: { main: "./src/scripts/main.js"},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: `${outputFile}.js`,
+    chunkFilename: `${outputFile}.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|svg|woff2?|tff|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: `${assetFile}.[ext]`,
+              outputPath: "images",
+              publicPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${outputFile}.css`,
+    }),
+    new ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery",
+      // utils: [path.resolve(__dirname, 'src/utils'),'default']
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 0,
+      // cacheGroups: {
+      //   Vendors: {
+      //     name: "vendors",
+      //     test: /[\\/]node_modules[\\/]/,
+      //     priority: -10,
+      //   },
+      //   // utils: {
+      //   //   name: "utils",
+      //   //   test: /src[\\/]utils/,
+      //   //   priority: -10,
+      //   // },
+      //   default: false,
+      // },
+    },
+  },
+});
